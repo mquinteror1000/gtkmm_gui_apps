3256f836d9788f9e217695baf56d8b6619e11f67
mquinteror1000 <martinsilloelfilosofo@gmail.com>
2026-06-19 20:59:22 -0600

    app hello w modifi un poco

--- 001_hello_worl_gui/main.cpp
+++ 001_hello_worl_gui/main.cpp
@@ -1,39 +1,34 @@
 #include <gtkmm.h>
 #include <iostream>
 
-// Heredamos de Gtk::Window para crear nuestra propia clase de ventana
 class MiVentana : public Gtk::Window {
 public:
     MiVentana() {
-        // Configuramos las propiedades de la ventana
-        set_title("Mi Primera App con GTK4");
-        set_default_size(300, 200);
+        // Propiedades de la ventana 
+        set_title("Mi Primera aplicacion con GTK4");
+        set_default_size(600, 400);
 
-        // Configuramos el botón
         m_button.set_label("¡Haz clic aquí!");
-        m_button.set_margin(50); // Un poco de espacio alrededor
+        m_button.set_margin(50); 
 
-        // Conectamos la señal 'clicked' del botón con nuestro método
         m_button.signal_clicked().connect(sigc::mem_fun(*this, &MiVentana::on_button_clicked));
 
-        // Añadimos el botón a la ventana
+        // Agregar el boton a la ventan 
         set_child(m_button);
     }
 
 protected:
-    // Nuestro manejador de la señal del botón
+    // Manejador de la señal de boton
     void on_button_clicked() {
-        std::cout << "¡Hola desde la terminal! El botón funciona." << std::endl;
-        m_button.set_label("¡Clickeado!");
+        std::cout << "hola, estamos trabajando con gtkmm 4.0" << std::endl;
+        m_button.set_label("Boton presionado");
     }
 
-    // Elementos de la interfaz
     Gtk::Button m_button;
 };
 
 int main(int argc, char* argv[]) {
-    // Inicializamos la aplicación GTK con un identificador único
-    auto app = Gtk::Application::create("org.martin.miapp");
+    auto app = Gtk::Application::create("org.my_user.hellowork");
 
     // Retornamos la ejecución de la aplicación, pasándole nuestra ventana
     return app->make_window_and_run<MiVentana>(argc, argv);
