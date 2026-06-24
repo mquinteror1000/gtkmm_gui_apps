81c6822127fd128f361d492f91ae99cafce7aabe
mquinteror1000 <martinsilloelfilosofo@gmail.com>
2026-06-24 15:39:29 -0600

    with no exec

--- 001_hello_worl_gui/compila.sh
+++
@@ -1,1 +0,0 @@
-g++ main.cpp -o mi_app $(pkg-config --cflags --libs gtkmm-4.0) -std=c++17
--- 003_occi_sample/compile.sh
+++
@@ -1,5 +0,0 @@
-g++ main.cpp -o mi_app_gtkmm \
-    `pkg-config gtkmm-4.0 --cflags --libs` \
-    -I/usr/include/oracle/23/client64 \
-    -L/usr/lib/oracle/23/client64/lib \
-    -locci -lclntsh
